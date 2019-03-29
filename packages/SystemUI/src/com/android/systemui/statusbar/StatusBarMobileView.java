3c38a61a757de8b4b9346f42d24ef70b4a4f84f4
umikaki <umikaki_y117@icloud.com>
2019-03-29 23:10:48 +0900

    SystemUI: Fixed padding of mobile data icon when mobile data icon is not wide type icon

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -123,7 +123,12 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mMobileType = findViewById(useOldStyle ? R.id.mobile_type_old : R.id.mobile_type);
         mMobileType.setVisibility(visOld);
         int wideTypeIconStartPadding = mContext.getResources().getDimensionPixelSize(R.dimen.wide_type_icon_start_padding);
-        mMobile.setPaddingRelative(useOldStyle ? wideTypeIconStartPadding : 0, 0, 0, 0);
+        mMobile.setPaddingRelative(
+          useOldStyle && (
+            mState.typeId != R.drawable.stat_sys_data_fully_connected_g &&
+            mState.typeId != R.drawable.stat_sys_data_fully_connected_e &&
+            mState.typeId != R.drawable.stat_sys_data_fully_connected_h )
+            ? wideTypeIconStartPadding : 0, 0, 0, 0);
     }
 
     private void initDotView() {
